2bc870897f93961c5037e6bf28fa81bd2aa9b022
rohit161206 <rohitks.ad24@bitsathy.ac.in>
2024-10-25 00:18:42 +0530

    day-3 task

---
+++ about/page.js
@@ -0,0 +1,12 @@
+export default function about(){
+    return (
+    <body class="body2">
+    <center>
+    <div class="div2">
+        <h2>𝓨𝓸𝓾 𝓱𝓪𝓿𝓮 𝓪𝓹𝓹𝓮𝓪𝓻𝓮𝓭 𝓸𝓷 𝓐𝓫𝓸𝓾𝓽 𝓹𝓪𝓰𝓮</h2>
+        <a href="/">𝔾𝕠 𝕥𝕠 ℍ𝕠𝕞𝕖 𝕡𝕒𝕘𝕖</a>
+    </div>
+    </center>
+    </body>
+    );
+};
---
+++ page.js
@@ -0,0 +1,12 @@
+export default function home(){
+    return(
+        <body class="body1">
+        <center>
+        <div class="div1">
+        <h1>𝓦𝓮𝓵𝓬𝓸𝓶𝓮 𝓗𝓸𝓶𝓮!</h1>
+        <a href="/about">𝔾𝕠 𝕥𝕠 𝕒𝕓𝕠𝕦𝕥</a>
+        </div>
+        </center>
+        </body>
+);
+};
